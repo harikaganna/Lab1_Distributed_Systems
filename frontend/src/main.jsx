d8859db24ccb592ac51c4d7ebffb3a2215e6da6b
Yaramsetty <harika.ganna@sjsu.edu>
2026-03-23 19:24:43 -0700

    more frontend changes

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AuthProvider } from "./context/AuthContext";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
